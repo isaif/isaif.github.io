a8411c23693e48e1ff1453d9aaf0a19e7ca8be5a
isaif <saif935775@gmail.com>
2020-08-25 18:58:50 +0530

    Updates

---
+++ precache-manifest.6e7770edc9e21f709421f5201b698e9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3d32ed68fd65639ddc90188e73b7e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb81d3d4edeba9184f7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4546a7d13d11aed512b0",
+    "url": "/static/js/2.31d152a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31d152a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb81d3d4edeba9184f7f",
+    "url": "/static/js/main.137ac2e0.chunk.js"
+  },
+  {
+    "revision": "b5e3031b331738911288",
+    "url": "/static/js/runtime-main.680723a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/isaif.github.io/precache-manifest.1e46e02a2b2ef302a98840721ade9702.js"
+  "/precache-manifest.6e7770edc9e21f709421f5201b698e9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/isaif.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
