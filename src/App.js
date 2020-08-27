2d2cef4c06316282825cb9d5c9112238a66cff1e
isaif <saif935775@gmail.com>
2020-08-27 14:33:33 +0530

    Add: routing for all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
 import './App.css';
+import Home from './Home.js';
+import Projects from './Projects.js';
+import About from './About.js';
+import Contact from './Contact.js';
+import NotFound from './NotFound.js';
 
 function App() {
   return (
     <div className="App">
+    <Router>
+      <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/projects">Projects</Link>
+      </li>
+      <li>
+        <Link to="about">About</Link>
+      </li>
+      <li>
+        <Link to="contact">Contact</Link>
+      </li>
+    </ul>
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+    </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
