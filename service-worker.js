c8d11e7c094c5e56f15324dbc761830ef50d1a1e
isaif <saif935775@gmail.com>
2020-09-01 18:43:39 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e7770edc9e21f709421f5201b698e9e.js"
+  "/precache-manifest.3c9ae85038a85014601fc0e4a89dfc8d.js"
 );
 
 self.addEventListener('message', (event) => {
