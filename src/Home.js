c046cd4c8e894e434da9057b8fd9bf56461a56b6
isaif <saif935775@gmail.com>
2020-09-02 22:22:29 +0530

    Add: links to bottom of Home component

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="Home">
       <div className="container">
-        <p>I'm Saif</p>
-        <h1>Web Developer</h1>
-        <p>Looking to create modern and aesthetically pleasing websites.</p>
+        <p className="intro-name">I'm Saif</p>
+        <h1 className="intro-main">Web Developer</h1>
+        <p className="intro-details">
+          Looking to create modern and aesthetically pleasing websites.
+        </p>
+      </div>
+      <div className="intro-links">
+        <Link to="/about">About</Link>
+        <span className="intro-links-divider">/</span>
+        <Link to="/projects">Projects</Link>
+        <span className="intro-links-divider">/</span>
+        <Link to="/contact">Contact</Link>
       </div>
     </div>
   );
