9c658b47ba57a82c1e5ece6f47267018d377028f
isaif <saif935775@gmail.com>
2020-09-02 13:43:55 +0530

    Modify: App.js to move path "/" to last place
    - recommended in reactrouter docs

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <SideBar />
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route path="/projects" component={Projects} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route exact path="/" component={Home} />
           <Route component={NotFound} />
         </Switch>
       </Router>
