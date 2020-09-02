3990f57391e73c71890b2e618d3be1ea0d61f716
isaif <saif935775@gmail.com>
2020-09-02 19:33:58 +0530

    Add: content in Home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return <div className="Home">Home</div>;
+  return (
+    <div className="Home">
+      <div className="container">
+        <p>I'm Saif</p>
+        <h1>Web Developer</h1>
+        <p>Looking to create modern and aesthetically pleasing websites.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
